5f75afb531ea56f1694e3e93be5f78a3e278c9f1
joonaskinnunen <joonas.kinn@gmail.com>
2022-01-18 18:46:07 +0200

    remove node_modules and add .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 let notes = [
   {"id": 1}
@@ -13,7 +16,7 @@ app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
